18754bed4300ef5e56293619e3e0fd1288bfb147
Pedro Theodoro <140471526+theodoro-pedro@users.noreply.github.com>
2024-04-23 00:09:38 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('registrar');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
